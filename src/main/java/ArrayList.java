bc68b88c958165ab446cb1f25819298db6d0fdb1
ALEX <super.vladius@yandex.ru>
2019-07-20 13:27:45 +0400

    1 ne hvataet

--- src/main/java/ArrayList.java
+++ src/main/java/ArrayList.java
@@ -5,8 +5,8 @@ public class ArrayList<T> {
     private Integer[] arr = new Integer[size];
     private int mark = 0;
 
-    public void resize() {
-        Integer[] copy = new Integer[arr.length * 2];
+    public void resize(int newLength) {
+        Integer[] copy = new Integer[newLength];
         for (int i = 0; i < arr.length; i++) {
             copy[i] = arr[i];
             arr = copy;
@@ -15,20 +15,26 @@ public class ArrayList<T> {
 
     public void add(Integer entity) {
         if (mark == arr.length - 1) {
-            resize();
+            resize(arr.length*2);
         }
         arr[mark++] = entity;
     }
 
-    public int get(int index) {
+    public int getIndex(int index) {
         return arr[index];
     }
 
+
     public void remove(int index) {
-        for (int i = index; i < mark; i++)
-            arr[i] = arr[i + 1];
+//        for (int i = index; i < mark; i++)
+//           arr[i] = arr[i + 1];
         arr[mark] = null;
         mark--;
+    }
+
+
+    public int getSize(){
+        return mark;
     }
 
 
